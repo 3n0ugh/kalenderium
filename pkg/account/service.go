2c989269055a3786a1ef5bdda9fc02b1172a7e13
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-20 02:04:46 +0300

    :sparkles: Update account service

--- pkg/account/service.go
+++ pkg/account/service.go
@@ -7,6 +7,7 @@ import (
 
 type Service interface {
 	IsAuth(ctx context.Context, token string) error
-	SignIn(ctx context.Context, user repository.User) (string, error)
+	SignUp(ctx context.Context, user repository.User) (string, error)
 	Login(ctx context.Context, user repository.User) (string, error)
+	Logout(ctx context.Context, token string) error
 }
