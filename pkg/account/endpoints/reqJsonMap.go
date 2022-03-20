f5c13c9b4fce8dae4d5443a96df4003d9106f1c1
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-20 05:06:45 +0300

    :sparkles: Add comments

--- pkg/account/endpoints/reqJsonMap.go
+++ pkg/account/endpoints/reqJsonMap.go
@@ -2,36 +2,44 @@ package endpoints
 
 import "github.com/3n0ugh/kalenderium/pkg/account/repository"
 
+// IsAuthRequest -> IsAuth endpoint's  input structures
 type IsAuthRequest struct {
 	Token string `json:"token"`
 }
 
+// IsAuthResponse -> IsAuth endpoint's output structure
 type IsAuthResponse struct {
-	Err error `json:"err"`
+	Err string `json:"err,omitempty"`
 }
 
+// SignUpRequest -> SignUp endpoint's  input structures
 type SignUpRequest struct {
 	User repository.User `json:"user"`
 }
 
+// SignUpResponse -> SignUp endpoint's output structure
 type SignUpResponse struct {
-	Token string `json:"token"`
-	Err   error  `json:"err"`
+	Token string `json:"token,omitempty"`
+	Err   string `json:"err,omitempty"`
 }
 
+// LoginRequest -> Login endpoint's  input structures
 type LoginRequest struct {
 	User repository.User `json:"user"`
 }
 
+// LoginResponse -> Login endpoint's output structure
 type LoginResponse struct {
-	Token string `json:"token"`
-	Err   error  `json:"err"`
+	Token string `json:"token,omitempty"`
+	Err   string `json:"err,omitempty"`
 }
 
+// LogoutRequest -> Logout endpoint's  input structures
 type LogoutRequest struct {
 	Token string `json:"token"`
 }
 
+// LogoutResponse -> Logout endpoint's output structure
 type LogoutResponse struct {
-	Err error `json:"err"`
+	Err string `json:"err,omitempty"`
 }
