f259ba5ca390745fddbb56d6e7d4d874226d879f
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-21 15:00:26 +0300

    :tada: Initialize calendar service

---
+++ pkg/calendar/service.go
@@ -0,0 +1,13 @@
+package calendar
+
+import (
+	"context"
+	"github.com/3n0ugh/kalenderium/pkg/calendar/repository"
+)
+
+type Service interface {
+	CreateEvent(ctx context.Context, event repository.Event, userId uint64) (uint64, error)
+	ListEvent(ctx context.Context, userId uint64) ([]*repository.Event, error)
+	DeleteEvent(ctx context.Context, eventId uint64) error
+	ServiceStatus(ctx context.Context) error
+}
