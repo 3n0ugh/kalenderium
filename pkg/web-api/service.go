8504f7c602b1979c8169a32cd876e2ddb5d85cc0
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-23 09:39:19 +0300

    :sparkles: Initialize web-api service

---
+++ pkg/web-api/service.go
@@ -0,0 +1,18 @@
+package web_api
+
+import (
+	"context"
+	"github.com/3n0ugh/kalenderium/internal/token"
+	repo "github.com/3n0ugh/kalenderium/pkg/account/repository"
+	"github.com/3n0ugh/kalenderium/pkg/calendar/repository"
+)
+
+type Service interface {
+	AddEvent(ctx context.Context, event repository.Event) (uint64, error)
+	ListEvent(ctx context.Context, userId uint64) ([]repository.Event, error)
+	DeleteEvent(ctx context.Context, eventId uint64, userId uint64) error
+
+	SignUp(ctx context.Context, user repo.User) (uint64, token.Token, error)
+	Login(ctx context.Context, user repo.User) (uint64, token.Token, error)
+	Logout(ctx context.Context, token token.Token) error
+}
---
+++ pkg/web-api/web-api.go
@@ -0,0 +1,193 @@
+package web_api
+
+import (
+	"context"
+	"fmt"
+	"github.com/3n0ugh/kalenderium/internal/token"
+	"github.com/3n0ugh/kalenderium/internal/validator"
+	pb2 "github.com/3n0ugh/kalenderium/pkg/account/pb"
+	repo "github.com/3n0ugh/kalenderium/pkg/account/repository"
+	"github.com/3n0ugh/kalenderium/pkg/calendar/pb"
+	"github.com/3n0ugh/kalenderium/pkg/calendar/repository"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type webApiService struct {
+	calendarClient pb.CalendarClient
+	accountClient  pb2.AccountClient
+}
+
+func NewWebApiService(calendarClient pb.CalendarClient, accountClient pb2.AccountClient) Service {
+	return &webApiService{
+		calendarClient: calendarClient,
+		accountClient:  accountClient,
+	}
+}
+
+func (w *webApiService) AddEvent(ctx context.Context, event repository.Event) (uint64, error) {
+	v := validator.New()
+	repository.ValidateEvent(v, event)
+	if !v.Valid() {
+		return 0, errors.New("failed to validate event")
+	}
+
+	pEvent := &pb.Event{
+		EventId:   event.EventId,
+		UserId:    event.UserId,
+		Title:     event.Title,
+		Body:      event.Body,
+		AttendAt:  timestamppb.New(event.AttendAt),
+		CreatedAt: timestamppb.New(event.CreatedAt),
+	}
+
+	resp, err := w.calendarClient.CreateEvent(ctx, &pb.CreateEventRequest{
+		Event: pEvent,
+	})
+
+	if err != nil {
+		return resp.EventId, errors.New(fmt.Sprintf("failed to validate event: %v", v.Errors))
+	}
+
+	return resp.EventId, nil
+}
+
+func (w *webApiService) ListEvent(ctx context.Context, userId uint64) ([]repository.Event, error) {
+	resp, err := w.calendarClient.ListEvent(ctx, &pb.ListEventRequest{
+		UserId: userId,
+	})
+
+	if err != nil {
+		return nil, errors.New("failed to list event")
+
+	}
+	var events []repository.Event
+	for _, e := range resp.Events {
+		event := repository.Event{
+			EventId:   e.EventId,
+			UserId:    e.UserId,
+			Title:     e.Title,
+			Body:      e.Body,
+			AttendAt:  e.AttendAt.AsTime(),
+			CreatedAt: e.CreatedAt.AsTime(),
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func (w *webApiService) DeleteEvent(ctx context.Context, eventId uint64, userId uint64) error {
+	_, err := w.calendarClient.DeleteEvent(ctx, &pb.DeleteEventRequest{
+		EventId: eventId,
+		UserId:  userId,
+	})
+
+	if err != nil {
+		return errors.New("failed to delete event")
+	}
+
+	return nil
+}
+
+func (w *webApiService) SignUp(ctx context.Context, user repo.User) (uint64, token.Token, error) {
+	err := user.Set(user.Password)
+	if err != nil {
+		return 0, token.Token{}, errors.Wrap(err, "failed to hash password")
+	}
+
+	v := validator.New()
+	repo.ValidateUser(v, &user)
+	if !v.Valid() {
+		return 0, token.Token{}, errors.New(fmt.Sprintf("failed to user: %v", v.Errors))
+	}
+
+	usr := pb2.User{
+		Email:    user.Email,
+		Password: user.Email,
+	}
+
+	resp, err := w.accountClient.SignUp(ctx, &pb2.SignUpRequest{User: &usr})
+	if err != nil {
+		return 0, token.Token{}, errors.Wrap(err, "failed to signup")
+	}
+
+	tkn := token.Token{
+		PlainText: resp.Token.PlaintText,
+		Hash:      resp.Token.Hash,
+		UserID:    resp.Token.UserId,
+		Expiry:    resp.Token.Expiry.AsTime(),
+		Scope:     resp.Token.Scope,
+	}
+	return resp.UserId, tkn, nil
+}
+
+func (w *webApiService) Login(ctx context.Context, user repo.User) (uint64, token.Token, error) {
+	err := user.Set(user.Password)
+	if err != nil {
+		return 0, token.Token{}, errors.Wrap(err, "failed to hash password")
+	}
+
+	v := validator.New()
+	repo.ValidateUser(v, &user)
+	if !v.Valid() {
+		return 0, token.Token{}, errors.New(fmt.Sprintf("failed to user: %v", v.Errors))
+	}
+
+	usr := pb2.User{
+		Email:    user.Email,
+		Password: user.Password,
+	}
+
+	resp, err := w.accountClient.Login(ctx, &pb2.LoginRequest{
+		User: &usr,
+	})
+	if err != nil {
+		return resp.UserId, token.Token{}, errors.New(resp.Err)
+	}
+
+	tkn := token.Token{
+		PlainText: resp.Token.PlaintText,
+		Hash:      resp.Token.Hash,
+		UserID:    resp.Token.UserId,
+		Expiry:    resp.Token.Expiry.AsTime(),
+		Scope:     resp.Token.Scope,
+	}
+
+	return resp.UserId, tkn, nil
+}
+func (w *webApiService) Logout(ctx context.Context, sToken token.Token) error {
+	v := validator.New()
+	token.ValidateTokenPlaintext(v, sToken.PlainText)
+	if !v.Valid() {
+		return errors.New(fmt.Sprintf("failed to validate token: %v", v.Errors))
+	}
+
+	tkn := &pb2.Token{
+		PlaintText: sToken.PlainText,
+		Hash:       sToken.Hash,
+		UserId:     sToken.UserID,
+		Expiry:     timestamppb.New(sToken.Expiry),
+		Scope:      sToken.Scope,
+	}
+
+	resp, err := w.accountClient.Logout(ctx, &pb2.LogoutRequest{Token: tkn})
+	if err != nil {
+		return errors.New(resp.Err)
+	}
+
+	return nil
+}
+
+func (w *webApiService) isAuth(ctx context.Context, sToken token.Token) error {
+	tkn := &pb2.Token{
+		PlaintText: sToken.PlainText,
+		Hash:       sToken.Hash,
+		UserId:     sToken.UserID,
+		Expiry:     timestamppb.New(sToken.Expiry),
+		Scope:      sToken.Scope,
+	}
+
+	_, err := w.accountClient.IsAuth(ctx, &pb2.IsAuthRequest{Token: tkn})
+	return err
+}
