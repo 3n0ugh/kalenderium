cd88437ee9a3a84f09709915777a26cb04f2af02
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-27 19:50:43 +0300

    :sparkles: Add Home page view

---
+++ web/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div class="home text-center">
+      <div class="jumbotron">
+        <h1 class="display-3">kalenderium</h1>
+        <p class="lead">Javascript is giving me pain but, it's also fun.</p>
+        <hr class="my-4" />
+        <p>Another hard-to-understand project.</p>
+        <p class="lead">
+          <router-link
+            to="/signup"
+            class="btn btn-primary btn-lg"
+            role="button"
+          >
+            Signup
+          </router-link>
+        </p>
+        <p class="lead">
+          <router-link to="/login" class="btn btn-primary btn-lg" role="button">
+            Login
+          </router-link>
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
