eec73651ce93a43711ddfc937dabe18a1634fc33
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-27 19:50:58 +0300

    :sparkles: Add Signup page view

---
+++ web/src/views/SignupView.vue
@@ -0,0 +1,15 @@
+<template>
+  <SignupComponent />
+</template>
+
+<script>
+import SignupComponent from "../components/SignupComponent";
+
+export default {
+  name: "SignupView",
+
+  components: {
+    SignupComponent,
+  },
+};
+</script>
