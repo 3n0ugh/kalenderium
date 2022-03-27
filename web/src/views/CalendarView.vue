dd7e0f699db5516c293206dd64b966d9e517180b
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-27 19:51:25 +0300

    :sparkles: Add Calendar page view

---
+++ web/src/views/CalendarView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <CalendarComponent />
+  </v-app>
+</template>
+
+<script>
+import CalendarComponent from "../components/CalendarComponent";
+
+export default {
+  name: "CalendarView",
+
+  components: {
+    CalendarComponent,
+  },
+};
+</script>
