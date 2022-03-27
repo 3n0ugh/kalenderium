6d394bec09e06e9025ecf7305384bb6b708774ca
3n0ugh <serhatkarabulut@protonmail.com>
2022-03-27 19:51:13 +0300

    :sparkles: Add Login page view

---
+++ web/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <LoginComponent />
+</template>
+
+<script>
+import LoginComponent from "../components/LoginComponent";
+
+export default {
+  name: "LoginView",
+
+  components: {
+    LoginComponent,
+  },
+};
+</script>
